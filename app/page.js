5feec791ca5d511dacfe20c49d879350b0020c81
guokenso <kenson.guo@mail.utoronto.ca>
2025-03-12 19:25:08 -0400

    normalize the page

--- app/page.js
+++ app/page.js
@@ -1,28 +1,42 @@
 // app/page.js
 export default function Home() {
   return (
-    <div className="w-[1769px] h-[1151px] relative bg-gradient-to-b from-[#db4439] to-[#75241e] overflow-hidden">
-      <div className="w-[150px] h-[52px] left-[1567px] top-[58px] absolute">
-        <div className="w-[150px] h-[52px] left-0 top-0 absolute bg-[#eaecd7] rounded-[49px]" />
-        <div className="left-[50px] top-0 absolute justify-start text-[#db4439] text-base font-black font-['Inknut_Antiqua']">Click</div>
-      </div>
-      <div className="origin-top-left rotate-[-8.64deg] w-[543px] h-[590.40px] left-[1068.69px] top-[226px] absolute">
-        <div className="w-[543px] h-[590.40px] left-[88.69px] top-0 absolute bg-[#d9d9d9]" />
-        <img className="w-[630.99px] h-[1054.71px] left-[81.51px] top-[-57.62px] absolute" src="https://placehold.co/631x1055" />
-      </div>
-      <div className="w-[680px] left-[143px] top-[313px] absolute justify-start text-[#eaecd7] text-[110px] font-black font-['Inknut_Antiqua'] leading-[134.20px]">This is the Topic This is the Topic</div>
-      <div className="w-[680px] left-[148px] top-[740px] absolute justify-start text-[#eaecd7] text-[40px] font-semibold font-['Inknut_Antiqua'] leading-[48.80px]">This is the Topic </div>
-      <div className="left-[270px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">Me</div>
-      <div className="left-[64px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">Front Page</div>
-      <div className="left-[891px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">ME</div>
-      <div className="left-[965px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">ME</div>
-      <div className="left-[716px] top-[46px] absolute justify-start text-[#eaecd7] text-2xl font-bold font-['Inknut_Antiqua']">Front Page</div>
-      <div className="w-[1804px] h-[102px] left-[-35px] top-[1049px] absolute bg-white" />
-      <div className="left-[124px] top-[1063px] absolute justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">This is the Topic</div>
-      <div className="left-[459px] top-[1063px] absolute justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">This is the Topic</div>
-      <div className="left-[794px] top-[1063px] absolute justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">This is the Topic</div>
-      <div className="left-[1464px] top-[1063px] absolute justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">This is the Topic</div>
-      <div className="left-[1129px] top-[1063px] absolute justify-start text-[#db4439] text-2xl font-black font-['Inknut_Antiqua'] leading-[29.28px]">This is the Topic</div>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-[#db4439] to-[#75241e] text-[#eaecd7]">
+      
+      {}
+      <header className="flex justify-between w-full max-w-5xl p-4">
+        <div className="text-2xl font-bold">Front Page</div>
+        <div className="text-2xl font-bold">About me</div>
+        <div className="text-2xl font-bold">Contact info</div>
+        <div className="text-2xl font-bold">Profile</div>
+      </header>
+
+      {}
+      <main className="flex flex-col items-center text-center gap-8 w-full max-w-3xl">
+        <h1 className="text-[50px] font-black leading-[134.20px]">
+          About me
+        </h1>
+
+        <h2 className="text-[40px] font-semibold leading-[48.80px]">
+         I am a utm student that want to complete this lab, I don't know what pic to put. I guess I will put my fav anime
+        </h2>
+
+        <div className="relative w-[200px] h-[280px] bg-[#d9d9d9] rounded-lg shadow-md">
+          <img 
+            className="w-full h-auto rounded-lg" 
+            src="https://i0.wp.com/anitrendz.net/news/wp-content/uploads/2024/08/spice-and-wolf-new-arc-key-visual-1-scaled.jpg?resize=696%2C984&ssl=1"
+          />
+        </div>
+
+        <button className="bg-[#eaecd7] text-[#db4439] px-6 py-3 rounded-full font-black">
+          Click
+        </button>
+      </main>
+
+      {}
+      <footer className="bg-white text-[#db4439] w-full text-center p-4 mt-8">
+        <p className="text-2xl font-black">Thank you for reading all this</p>
+      </footer>
     </div>
   );
 }
